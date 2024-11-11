da3dd0b1fbc6cc6e127e37c61304dc326b2946ec
Ashot08 <albertguzeev@yandex.ru>
2024-11-11 22:40:57 +0700

    add webpack build destructorisation

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,17 @@
 import path from "path";
 import webpack from 'webpack';
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildOptions, BuildPaths } from './config/types/config';
 
-const config: webpack.Configuration = {
-    mode: 'development',
+const options: BuildOptions = {
+  paths: {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-        new webpack.ProgressPlugin(),
-    ]
-};
+    outputPath: path.resolve(__dirname, 'dist'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  },
+  mode: 'development',
+}
+
+const config = buildWebpackConfig(options);
 
 export default config;

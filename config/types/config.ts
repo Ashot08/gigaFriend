681c3f188d90f746ead595d534b19901d7767b7d
Ashot08 <albertguzeev@yandex.ru>
2024-11-17 00:25:45 +0700

    add dev server

--- config/types/config.ts
+++ config/types/config.ts
@@ -1,4 +1,4 @@
-export type BuildMode = 'production' | 'development';
+export type BuildMode = BuildModes.Production | BuildModes.Development;
 export interface BuildPaths {
   entry: string;
   outputPath: string;
@@ -7,4 +7,16 @@ export interface BuildPaths {
 export interface BuildOptions {
   mode: BuildMode;
   paths: BuildPaths;
+  isDev: boolean;
+  port: string | number;
+}
+
+export interface EnvironmentOptions {
+  port: number | string,
+  mode: BuildMode,
+}
+
+export enum BuildModes {
+  Production = 'production',
+  Development = 'development',
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,25 @@
 import path from "path";
-import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildOptions, BuildPaths } from './config/types/config';
+import { BuildModes, BuildOptions, BuildPaths, EnvironmentOptions } from './config/types/config';
+import { WebpackConfiguration } from 'webpack-cli';
 
-const options: BuildOptions = {
-  paths: {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    outputPath: path.resolve(__dirname, 'dist'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-  },
-  mode: 'development',
+export default (env: EnvironmentOptions) => {
+  const mode = env.mode || BuildModes.Development;
+  const isDev = mode === BuildModes.Development;
+  const PORT = env.port || 8000;
+
+  const options: BuildOptions = {
+    paths: {
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      outputPath: path.resolve(__dirname, 'dist'),
+      html: path.resolve(__dirname, 'public', 'index.html'),
+    },
+    mode,
+    isDev,
+    port: PORT,
+  }
+  const config: WebpackConfiguration = buildWebpackConfig(options);
+  return config;
 }
 
-const config = buildWebpackConfig(options);
 
-export default config;
